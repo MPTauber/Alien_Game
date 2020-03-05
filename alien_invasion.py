cc819a858bb9809d33a53ecd1b7166432f625f2b
Tauber <Madeleine_Tauber1@baylor.edu>
2020-03-05 17:35:47 -0600

    fix bug in spelling

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,7 +81,7 @@ class AlienInvasion:
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
         screen_rect = self.screen.get_rect()
-        for aliens in self.aliens.sprites():
+        for alien in self.aliens.sprites():
             if alien.rect.bottom >= screen_rect.bottom:
                 #Treat this the same as if the ship got hit.
                 self._ship_hit()
