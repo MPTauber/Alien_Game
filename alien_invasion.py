0139afa16d9a2e6ddcb8a3384f84ab9e19f219f7
Tauber <Madeleine_Tauber1@baylor.edu>
2020-03-05 16:44:47 -0600

    refactoring _update_bulelts()

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,7 +87,11 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        
+
+        self._check_bullet_alien_collisions()
+    
+    def _check_bullet_alien_collisions():
+        """Respond to bullet-alien collisions."""
         #Check for any bullets that have hit aliens.
         # if so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(
