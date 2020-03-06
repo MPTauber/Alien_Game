666228e60ca5edece924296024d48a9b2265c98a
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-03-06 16:08:52 -0600

    making sure to score all hits

--- alien_invasion.py
+++ alien_invasion.py
@@ -78,6 +78,7 @@ class AlienInvasion:
             #Reset the game stats.
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             #get rid of remaining aliens and bullets.
             self.aliens.empty()
